8843a53da221ac0ee15c9395cd03c55dcb32dd62
cckim <r2222911r@ajou.ac.kr>
2023-10-30 22:26:19 +0900

    상하좌우

--- thiscodingtest044_1.py
+++ thiscodingtest044_1.py
@@ -4,22 +4,17 @@ command = list(input().split())
 for j in command:
 
     if j == 'R':
-        if second == n-1:
-            continue
-        second += 1
+        if second != n-1:
+            second += 1
     elif j == 'L':
-        if second == 0:
-            continue
-        second -= 1
+        if second != 0:
+            second -= 1
     elif j == 'U':
-        if first == 0:
-            continue
-        first -= 1
+        if first != 0:
+            first -= 1
     elif j == 'D':
-        if first == n-1:
-            continue
-        first +=1 
-    
+        if first != n-1:
+            first +=1 
 
 print(first+1, second+1)
         
