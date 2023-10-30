e1ed2c8653077ea2613e05a4d1c00fb29716bf51
cckim <r2222911r@ajou.ac.kr>
2023-10-30 22:55:28 +0900

    시각

---
+++ thiscodingtest044_2.py
@@ -0,0 +1,8 @@
+n = int(input())
+result = 0
+for i in range(0,n+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i)+str(j)+str(k):
+                result += 1
+print(result)
